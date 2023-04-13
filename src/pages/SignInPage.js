744b6df09d2509c836cfe2daf178f6b0dbe6db0d
Kourosh N. Baygi <kourosh824@gmail.com>
2023-04-14 01:57:32 +0300

    Added styles for the SignInPage

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from "../database/firebase";
 
+import logo from '../images/ntua-logo.svg';
+import signInStyles from '../styles/signInPage.module.css';
+
 const SignInPage = () => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
@@ -20,30 +23,35 @@ const SignInPage = () => {
     };
 
     return (
-        <div>
-            <div>
-                <p>
-                    Email
+        <div
+        className={signInStyles['signin']}>
+            <div
+            className={signInStyles['signin__header']}>
+                <img
+                className={signInStyles['signin__logo']}
+                alt="Εθνικό Μετσόβιο Πολυτεχνείο"
+                src={logo} />
+                <p
+                className={signInStyles['signin__title']}>
+                    NTUAssist
                 </p>
-                <input
-                type="email"
-                name="email"
-                placeholder="email"
-                required
-                onChange={(e) => setEmail(e.target.value)} />
             </div>
-            <div>
-                <p>
-                    Password
-                </p>
-                <input
-                type="password"
-                name="password"
-                placeholder="password"
-                required
-                onChange={(e) => setPass(e.target.value)} />
-            </div>
+            <input
+            className={signInStyles['signin__input']}
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            required
+            onChange={(e) => setEmail(e.target.value)} />
+            <input
+            className={signInStyles['signin__input']}
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+            onChange={(e) => setPass(e.target.value)} />
             <button
+            className={signInStyles['signin__submit']}
             onClick={handleSubmit}>
                 Submit
             </button>
